e80dff7d8e4f2ca04c090b7188f5fcbbb93dc73a
CORMAC SOMERVILLE <cormacsomerville@outlook.com>
2022-10-07 20:13:06 +0100

    [+Added] Script and CharacterControler

---
+++ Assets/Scripts/BaseCharacterController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterController : MonoBehaviour
+{
+    public CharacterController characterController;
+    public Vector2 moveInput; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterController
+    }
+}
