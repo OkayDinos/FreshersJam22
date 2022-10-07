5f162d42ecd4c59b2c1b672b37993effe767d0ef
Cyp P <cypriane81@gmail.com>
2022-10-08 00:40:07 +0100

    [+Added] Sized tiles + moving world

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -4,25 +4,79 @@ using UnityEngine;
 
 public class WorldManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> prefabTilesList = new List<GameObject>();
+    [SerializeField] List<WorldTile> prefabTilesList = new List<WorldTile>();
 
-    List<GameObject> tilesList = new List<GameObject>();
+    List<PlacedWorldTile> tilesList = new List<PlacedWorldTile>();
+
+    int worldSize;
+
+    [SerializeField] float worldCentre;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 spawnPos = new Vector3(0,0,0);
-        for (int i = 0; i < 3; i++)
+        GenerateWorld();
+    }
+
+    void GenerateWorld()
+    {
+        worldSize = 0;
+
+        int targetWorldSize = 20;
+        
+        Vector3 nextTileSpawnPos = new Vector3(0, 0, 0);
+
+        while (targetWorldSize > worldSize)
         {
-            GameObject tile = Instantiate(prefabTilesList[0], spawnPos, Quaternion.identity);
-            tilesList.Add(tile);
-            spawnPos.x += 10;
+            int random = Random.Range(0, prefabTilesList.Count);
+
+            Vector3 currentTileSpawnPos = nextTileSpawnPos + new Vector3(((prefabTilesList[random].tileLength - 1) * 10) / 2, 0, 0);
+            GameObject tile = Instantiate(prefabTilesList[random].prefabTile, currentTileSpawnPos, Quaternion.identity);
+            tilesList.Add(new PlacedWorldTile { tile = tile, length = prefabTilesList[random].tileLength });
+            nextTileSpawnPos.x += 10 * prefabTilesList[random].tileLength;
+
+            worldSize += prefabTilesList[random].tileLength;
+        }
+
+        worldCentre = (worldSize * 10) / 2;
+    }
+
+    void MoveTile(bool _rightDirection) // false = left direction
+    {
+        if (_rightDirection)
+        {
+            PlacedWorldTile movedTile = tilesList[0];
+            tilesList.RemoveAt(0);
+
+            Vector3 endPosition = tilesList[tilesList.Count - 1].tile.transform.position + new Vector3(((tilesList[tilesList.Count - 1].length - 1) * 10) / 2, 0, 0);
+
+            movedTile.tile.transform.position = new Vector3(endPosition.x + 10 + (((movedTile.length - 1) * 10) / 2), 0, 0);
+
+            tilesList.Add(movedTile);
+        }
+        else
+        {
+            PlacedWorldTile movedTile = tilesList[tilesList.Count - 1];
+            tilesList.RemoveAt(tilesList.Count - 1);
+
+            Vector3 endPosition = tilesList[0].tile.transform.position - new Vector3(((tilesList[0].length - 1) * 10) / 2, 0, 0);
+
+            movedTile.tile.transform.position = new Vector3(endPosition.x - 10 - (((movedTile.length - 1) * 10) / 2), 0, 0);
+
+            tilesList.Insert(0, movedTile);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (tilesList[0].tile.transform.position.x < worldCentre - ((worldSize + 1) * 10 / 2))
+        {
+            MoveTile(true);
+        }
+        else if (tilesList[tilesList.Count - 1].tile.transform.position.x > worldCentre + ((worldSize + 1) * 10 / 2))
+        {
+            MoveTile(false);
+        }
     }
 }
