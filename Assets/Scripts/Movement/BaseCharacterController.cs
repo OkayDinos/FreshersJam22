8908010de61d385353fe88d9333c5b58652f198a
CORMAC SOMERVILLE <cormacsomerville@outlook.com>
2022-10-07 20:48:44 +0100

    [+Added] Character test Scene [+Added] miligrams of code

--- Assets/Scripts/Movement/BaseCharacterController.cs
+++ Assets/Scripts/Movement/BaseCharacterController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BaseCharacterController : MonoBehaviour
 {
+
+    // my whole work = variables :D
+    public SimpleControls Input;
+    public Vector2 velocity;
+    public float speed; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +22,5 @@ public class BaseCharacterController : MonoBehaviour
     {
         
     }
+
 }
