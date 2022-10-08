0a0672bbeb53a75d5d35c99c45d7349408e6ff9e
CORMAC SOMERVILLE <cormacsomerville@outlook.com>
2022-10-08 15:58:49 +0100

    [+Added] Smoother Camera Movement

--- Assets/Scripts/Movement/BaseCharacterController.cs
+++ Assets/Scripts/Movement/BaseCharacterController.cs
@@ -26,6 +26,7 @@ public class BaseCharacterController : MonoBehaviour
     float timeSinceLastFrame; // time since last frame alternate
     int currentFrame; // All animations bear 2 frames this inicates which one we are on 
     bool usedFlap, flapActive; // true if player has flapped this jump | if space let go mid air then allow flap
+    public float cameraSpeed; // Speed of the cmera moving 
     //float previousXMove;
     public Transform cameraTransform;
 
@@ -66,11 +67,11 @@ public class BaseCharacterController : MonoBehaviour
 
         // Jump Action
         //Check if flap allowed. (must let go of jump button and press again to flap)
-        if (jumpAction.ReadValue<float>() == 0 && !IsGrounded()) { 
+        if (jumpAction.ReadValue<float>() == 0 && !IsGrounded()) {
             flapActive = true;
         }
         //bool groundedAtTimeOfJump = IsGrounded();
-        if (jumpAction.ReadValue<float>() == 1 && (IsGrounded() || (!usedFlap && flapActive))) 
+        if (jumpAction.ReadValue<float>() == 1 && (IsGrounded() || (!usedFlap && flapActive)))
         {
             //Flapping not allowed until jump button released
             flapActive = false;
@@ -85,39 +86,39 @@ public class BaseCharacterController : MonoBehaviour
             if (xMove != 0) {
                 // Little funky calculation stops extreme launches
                 horizSpeed = horizSpeed / desiredSpeed * desiredSpeed;
-                //depthSpeed = depthSpeed / desiredZSpeed * desiredZSpeed;
             }
             if (zMove != 0)
             {
                 // Little funky calculation stops extreme launches
-                //horizSpeed = horizSpeed / desiredSpeed * desiredSpeed;
                 depthSpeed = depthSpeed / desiredZSpeed * desiredZSpeed;
             }
-            
+
 
         }
         // Gliding mechanic 
         //float glideAccel = 0;
         if (!IsGrounded() && xMove != 0 && kevinRigidbody.velocity.y <= 0) {
             playerSprite.sprite = animationSprites[7];
-            
+
             // Allow sprite flip while gliding 
             if (xMove > 0)
             {
                 playerSprite.flipX = false;
             } else if (xMove < 0) { playerSprite.flipX = true; }
-            
-            
+
+
             kevinRigidbody.useGravity = false;
             //glideAccel = glideGravity * Time.deltaTime;
             kevinRigidbody.velocity = new Vector3(kevinRigidbody.velocity.x, glideSpeed, kevinRigidbody.velocity.z);
         } else { kevinRigidbody.useGravity = true; }
+
         if (!IsGrounded() && xMove == 0 && kevinRigidbody.velocity.y <= 0)
-        { playerSprite.sprite = animationSprites[2]; //Walk 1 as fall sprite
+        {
+            playerSprite.sprite = animationSprites[2]; //Walk 1 as fall sprite
         }
 
-            // Change sprite facing direction
-            if (xMove > 0 && IsGrounded())
+        // Change sprite facing direction
+        if (xMove > 0 && IsGrounded())
         {
             playerSprite.flipX = false;
         }
@@ -125,14 +126,14 @@ public class BaseCharacterController : MonoBehaviour
             playerSprite.flipX = true;
         }
 
-        if (xMove == 0 && IsGrounded()) { 
+        if (xMove == 0 && IsGrounded()) {
             playerSprite.sprite = animationSprites[currentFrame];
         }
-        else if (xMove != 0 && IsGrounded()) { 
-            playerSprite.sprite = animationSprites[currentFrame+1]; 
+        else if (xMove != 0 && IsGrounded()) {
+            playerSprite.sprite = animationSprites[currentFrame + 1];
         }
 
-        if (timeSinceLastFrame >= (0.4 * (maxSpeed/(Mathf.Abs(horizSpeed)+maxSpeed)))) {
+        if (timeSinceLastFrame >= (0.4 * (maxSpeed / (Mathf.Abs(horizSpeed) + maxSpeed)))) {
             currentFrame = currentFrame == 1 ? 0 : 1;
             timeSinceLastFrame = 0;
         }
@@ -148,11 +149,11 @@ public class BaseCharacterController : MonoBehaviour
             horizSpeed += acceleration * airControlMultipler * Time.deltaTime; //Execute the acceleration
             depthSpeed += depthAcceleration * airControlMultipler * Time.deltaTime;
         }
-        
+
 
         horizSpeed = Mathf.Clamp(horizSpeed, -maxSpeed, maxSpeed); //Clamp the speed at the max speed
         depthSpeed = Mathf.Clamp(depthSpeed, -maxZSpeed, maxZSpeed); //Clamp the speed at the max speed
-        
+
         //Stop Velocity at X collisions
         if (Physics.Raycast(transform.position, Vector3.right, distToEdge + 0.1f) || Physics.Raycast(transform.position - Vector3.up * (distToGround - 0.1f), Vector3.right, distToEdge + 0.1f) || Physics.Raycast(transform.position + Vector3.up * distToGround, Vector3.right, distToEdge + 0.1f)) { horizSpeed = horizSpeed > 0 ? 0 : horizSpeed; } // stop horizontal velocity when going right
         if (Physics.Raycast(transform.position, -Vector3.right, distToEdge + 0.1f) || Physics.Raycast(transform.position - Vector3.up * (distToGround - 0.1f), -Vector3.right, distToEdge + 0.1f) || Physics.Raycast(transform.position + Vector3.up * distToGround, -Vector3.right, distToEdge + 0.1f)) { horizSpeed = horizSpeed < 0 ? 0 : horizSpeed; ; } // stop horizontal velocity when going left
@@ -168,13 +169,11 @@ public class BaseCharacterController : MonoBehaviour
 
         //Transfer horizontal velocity onto rigid body
         kevinRigidbody.velocity = (new Vector3(horizSpeed, kevinRigidbody.velocity.y, depthSpeed));
-        
+
         // Set Velocity to the public vector3 for viewing in the engine.
         velocity = kevinRigidbody.velocity;
 
-        // Set camera position
-        cameraTransform.position = new Vector3(this.transform.position.x, 3, -10);
-
+        
         // Add DT to time of last frame
         timeSinceLastFrame += Time.deltaTime;
 
@@ -182,7 +181,23 @@ public class BaseCharacterController : MonoBehaviour
         //previousXMove = xMove;
         
     }
-
+    private void FixedUpdate()
+    {
+        // Set camera position
+        if (this.transform.position.x >= cameraTransform.position.x + 3)
+        {
+            //cameraTransform.position = new Vector3(this.transform.position.x - 3, 3, -10);
+            Vector3 targCamPos = new Vector3(this.transform.position.x, 3, -10);
+            //Vector3 camVelocity = new Vector3(3, 0, 0);//Mathf.Abs(targCamPos.x - cameraTransform.position.x)
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, targCamPos, cameraSpeed * Time.deltaTime);
+        }
+        else if (this.transform.position.x <= cameraTransform.position.x - 3)
+        {
+            Vector3 targCamPos = new Vector3(this.transform.position.x, 3, -10);
+            //Vector3 camVelocity = new Vector3(targCamPos.x - cameraTransform.position.x - 3, 0, 0);
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, targCamPos, cameraSpeed * Time.deltaTime);//Vector3.SmoothDamp(cameraTransform.position, targCamPos, ref camVelocity, 0.3f);
+        }
+    }
     // Little ground checker.
     bool IsGrounded() {
         return (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f) || Physics.Raycast(transform.position - Vector3.right * distToEdge - Vector3.forward * distToDepthEdge, -Vector3.up, distToGround + 0.1f) || Physics.Raycast(transform.position + Vector3.right * distToEdge - Vector3.forward * distToDepthEdge, -Vector3.up, distToGround + 0.1f) || Physics.Raycast(transform.position - Vector3.right * distToEdge + Vector3.forward * distToDepthEdge, -Vector3.up, distToGround + 0.1f) || Physics.Raycast(transform.position + Vector3.right * distToEdge + Vector3.forward * distToDepthEdge, -Vector3.up, distToGround + 0.1f));
