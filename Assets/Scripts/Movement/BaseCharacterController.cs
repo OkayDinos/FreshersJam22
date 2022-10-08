58c8ff2a7fe7c91d4ceb58082a6aef7fe73737a7
CORMAC SOMERVILLE <cormacsomerville@outlook.com>
2022-10-08 11:19:46 +0100

    [+Added] Depth Z Axis Movement

--- Assets/Scripts/Movement/BaseCharacterController.cs
+++ Assets/Scripts/Movement/BaseCharacterController.cs
@@ -10,11 +10,12 @@ public class BaseCharacterController : MonoBehaviour
     public InputAction moveAction; //Move inputs
     public InputAction jumpAction; //Jump input
     public Vector3 velocity; // Velocity 
-    public float maxSpeed; // The maximum speed you can run 
+    public float maxSpeed, maxZSpeed; // The maximum speed you can run 
+    public Vector2 zBoundaries; // boundaries for the character on the z plane
     public float accelMultiplier; // How fast you reach the max speed 
     public float jumpVelocity; // how much ms-1 you jump up at
-    float horizSpeed; // Velocity on the X axis
-    float desiredSpeed; // Desired speed of travel, so if half out on joystick, half of max speed
+    float horizSpeed, depthSpeed; // Velocity on the X axis
+    float desiredSpeed, desiredZSpeed; // Desired speed of travel, so if half out on joystick, half of max speed
     Rigidbody kevinRigidbody; // Rigidbody of the character
     //Transform playerTransform; // Transform of the character
     Collider playerCollider; // Collider of the character
@@ -40,47 +41,61 @@ public class BaseCharacterController : MonoBehaviour
     void Update()
     {
 
-        // Get the X - Axis input
-        var moveDirection = moveAction.ReadValue<float>();
+        // Get the X/Z - Axis input
+        var moveDirection = moveAction.ReadValue<Vector2>();
+        var xMove = moveDirection.x;
+        var zMove = moveDirection.y;
 
         // Calculate speed accounting for changes in desired speed (maxSpeed * moveDirection)
 
-        desiredSpeed = maxSpeed * moveDirection; //Get target speed
+        desiredSpeed = maxSpeed * xMove; //Get target speed
+        desiredZSpeed = maxZSpeed * zMove;
 
         // Jump Action
         if (jumpAction.ReadValue<float>() == 1 && IsGrounded())
         {
             kevinRigidbody.velocity = new Vector3(kevinRigidbody.velocity.x, jumpVelocity, kevinRigidbody.velocity.z);
             // If wanting to move left/right launch that way
-            if (moveDirection != 0) {
+            if (xMove != 0) {
                 // Little funky calculation stops extreme launches
                 horizSpeed = horizSpeed / desiredSpeed * desiredSpeed;
+                depthSpeed = depthSpeed / desiredZSpeed * desiredZSpeed;
             }
         
         }
 
         // Change sprite facing direction
-        if (moveDirection > 0 && IsGrounded())
+        if (xMove > 0 && IsGrounded())
         {
             playerSprite.flipX = false;
         }
-        else if (moveDirection < 0 && IsGrounded()) {
+        else if (xMove < 0 && IsGrounded()) {
             playerSprite.flipX = true;
         }
 
-            float acceleration = desiredSpeed > horizSpeed ? accelMultiplier : -accelMultiplier; //Get acceleration needed to get there
+        float acceleration = desiredSpeed > horizSpeed ? accelMultiplier : -accelMultiplier; //Get acceleration needed to get there
+        float depthAcceleration = desiredZSpeed > depthSpeed ? accelMultiplier : -accelMultiplier; //Get acceleration needed to get there
         // Only accelerate if on the ground. 
         if (IsGrounded())
         {
             horizSpeed += acceleration * Time.deltaTime; //Execute the acceleration
+            depthSpeed += depthAcceleration * Time.deltaTime;
         }
+
         horizSpeed = Mathf.Clamp(horizSpeed, -maxSpeed, maxSpeed); //Clamp the speed at the max speed
+        depthSpeed = Mathf.Clamp(depthSpeed, -maxZSpeed, maxZSpeed); //Clamp the speed at the max speed
         
         if (Physics.Raycast(transform.position, Vector3.right, distToEdge + 0.1f)) { horizSpeed = horizSpeed > 0 ? 0 : horizSpeed; } // stop horizontal velocity when going right
         if (Physics.Raycast(transform.position, -Vector3.right, distToEdge + 0.1f)) { horizSpeed = horizSpeed < 0 ? 0 : horizSpeed; ; } // stop horizontal velocity when going left
-        //Transfer horizontal velocity onto rigid body
-        kevinRigidbody.velocity = (new Vector3(horizSpeed, kevinRigidbody.velocity.y, kevinRigidbody.velocity.z));
 
+        // Stops Z being more or less than max and min
+        float clampedZ;
+        clampedZ = Mathf.Clamp(this.transform.position.z, zBoundaries.x, zBoundaries.y);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, clampedZ);
+
+        //Transfer horizontal velocity onto rigid body
+        kevinRigidbody.velocity = (new Vector3(horizSpeed, kevinRigidbody.velocity.y, depthSpeed));
+        
         // Set Velocity to the public vector3 for viewing in the engine.
         velocity = kevinRigidbody.velocity;
 
