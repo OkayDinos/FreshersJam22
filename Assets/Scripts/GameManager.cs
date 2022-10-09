e8d611c805e5ba0a4ad909c64c0f88ca0ad1d505
Morgan Finney | pdox <dev@morganfinney.com>
2022-10-09 11:07:45 +0100

    [+Added] Pause Menu Logic (Player and Grans now need to check if GameManager.instance.currentGameState = GameState.Playing)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState { Playing, Paused, Ended }
+
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     float score;
+
+    public GameState currentGameState = GameState.Playing;
     
     // Start is called before the first frame update
     void Start()
     {
+        if (GameManager.instance)
+            Destroy(this);
+        else
+            GameManager.instance = this;
+
         StartGame();
     }
 
@@ -21,5 +32,11 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void DoPauseGame()
+    {
+        currentGameState = GameState.Paused;
+        PauseMenu.instance.gameObject.SetActive(true);
     }
 }
